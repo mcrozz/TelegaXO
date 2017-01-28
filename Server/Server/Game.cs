97119a821bbe85e04d72c775f8fa09dddd6b5765
Rozz <ivanzarudny@gmail.com>
2017-01-28 23:16:59 +0500

    Extracting data from callbackQuery, starting new game and call updateState

--- Server/Server/Game.cs
+++ Server/Server/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Server
 {
@@ -8,16 +9,15 @@ namespace Server
 
         public static void Start()
         {
-            Telegram.Start(ref telegram);
+            Telegram.Start(ref _telegram);
         }
 
-        // TODO: implement for PvP
-        public static void StartGame(DTO.Telegram.User player1, DTO.Telegram.User player2)
+        public static void StartGame(DTO.Telegram.User player1, DTO.Telegram.User player2 = null)
         {
-        }
-
-        public static void StartGame(DTO.Telegram.User player)
-        {
+            Player _player1 = new Player(player1);
+            Player _player2 = new Player(player2);
+            Room room = new Room(_player1, _player2);
+            _rooms.Add(room);
         }
 
         public static void ParseNormalMessage(DTO.Telegram.Message message)
@@ -31,7 +31,7 @@ namespace Server
                 switch (command)
                 {
                     case "start":
-                        // TODO: StartGame
+                        StartGame(message.@from);
                         break;
                     default:
                         break;
@@ -101,11 +101,25 @@ namespace Server
         {
             Console.WriteLine($"Got callbackQuery with data: {query.data}");
 
-            // TODO: forward it to the Room[].UpdateState
+            Boolean foundRoom = false;
+            foreach (Room room in _rooms)
+            {
+                if (room.isPlayerPresent(query.@from.id))
+                {
+                    Match point = extractData.Match(query.data);
+                    if (!point.Success)
+                        throw new Exception(ExcDataNotExctracted);
+                    room.UpdateState(query.@from.id, (Int16)point.Groups[0], (Int16)point.Groups[1]);
+                    foundRoom = true;
+                }
+            }
+            if (foundRoom)
+                return;
+
             DTO.Telegram.AnswerCallbackQuery answerQuery = new DTO.Telegram.AnswerCallbackQuery
             {
                 callback_query_id = query.id,
-                text = "Your anser accepted"
+                text = ExcRoomNotFound
             };
             DTO.Message reply = new DTO.Message
             {
@@ -115,7 +129,12 @@ namespace Server
             Telegram.Send(reply);
         }
 
-        private static IList<Room> rooms = new List<Room>();
-        private static Telegram telegram = new Telegram();
+        private static IList<Room> _rooms = new List<Room>();
+        private static Telegram _telegram = new Telegram();
+
+        private static readonly Regex extractData = new Regex(@"^(\d)_(\d)$");
+
+        private const String ExcRoomNotFound = "Maybe you wanted to begin game?";
+        private const String ExcDataNotExctracted = "Could not extract data from callbackQuery";
     }
 }
