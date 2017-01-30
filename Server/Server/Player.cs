173a123a62ca29839933c97a7294a13dc8750625
floatGOAT <pandarpr@yandex.ru>
2017-01-30 19:20:42 +0600

    Start developing class "IPlayer"
    Not-working version. First steps.

--- Server/Server/Player.cs
+++ Server/Server/Player.cs
@@ -1,9 +1,30 @@
-﻿
+﻿// Newton library
 namespace Server
 {
-    class Player
+    abstract class IPlayer
     {
-        Player() { }
-        public void SendMessage() { }
+        public IPlayer(DTO.Telegram.User player)
+        {
+            this.id = player.id;
+        }
+        public abstract void SendMessage(string text);
+        //public abstract void SendMessage(IDictionary<Int8, Int8> obj);
+        public System.Int32 getID()
+        {
+            return this.id;
+        }
+
+        //private DTO.Telegram.User Personality
+        private System.Int32 id;
+    };
+
+    class Player : IPlayer
+    {
+        public override void SendMessage(string text)
+        {
+            DTO.Message reply = new DTO.Message();
+            reply.Text = text;
+            Telegram.Send(reply);
+        }
     }
 }
