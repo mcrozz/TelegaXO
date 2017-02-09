1e1f081ddd5e7be4cce5c305439b6c75f1bf1db6
floatGOAT <pandarpr@yandex.ru>
2017-02-09 18:10:15 +0600

    Implemet user part of "IPlayer" interface

--- Server/Server/Player.cs
+++ Server/Server/Player.cs
@@ -1,30 +1,69 @@
-﻿// Newton library
+﻿using System;
+
 namespace Server
 {
-    abstract class IPlayer
+    class Player : IPlayer
     {
-        public IPlayer(DTO.Telegram.User player)
+        public Player(DTO.Telegram.User player)
         {
             this.id = player.id;
         }
-        public abstract void SendMessage(string text);
-        //public abstract void SendMessage(IDictionary<Int8, Int8> obj);
-        public System.Int32 getID()
+        public System.Int32 ID
         {
-            return this.id;
+            get { return id; }
         }
-
-        //private DTO.Telegram.User Personality
-        private System.Int32 id;
-    };
-
-    class Player : IPlayer
-    {
-        public override void SendMessage(string text)
+        public void SendMessage(Room.Fill[,] cell)
         {
-            DTO.Message reply = new DTO.Message();
-            reply.Text = text;
+            Int32 size = cell.GetLength(0);
+            DTO.Telegram.InlineKeyboardButton[,] buttons = new DTO.Telegram.InlineKeyboardButton[size, size];
+            for (Int32 i = 0; i < size; i++)
+            {
+                for (Int32 j = 0; j < size; j++)
+                {
+                    switch (cell[i, j])
+                    {
+                        case Room.Fill.empty:
+                            buttons[i, j] = new DTO.Telegram.InlineKeyboardButton(" ", $"{i}_{j}");
+                            break;
+                        case Room.Fill.tic:
+                            buttons[i, j] = new DTO.Telegram.InlineKeyboardButton("X", $"{i}_{j}");
+                            break;
+                        case Room.Fill.tac:
+                            buttons[i, j] = new DTO.Telegram.InlineKeyboardButton("O", $"{i}_{j}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            DTO.Telegram.InlineKeyboardMarkup keyboard = new DTO.Telegram.InlineKeyboardMarkup
+            {
+                inline_keyboard = buttons
+            };
+            DTO.Message reply = new DTO.Message
+            {
+                User = new DTO.Telegram.User
+                {
+                    id = this.ID
+                },
+                KeyboardMarkup = keyboard
+            };
             Telegram.Send(reply);
         }
+        public void SendMessage(string text)
+        {
+            DTO.Message reply = new DTO.Message
+            {
+                User = new DTO.Telegram.User
+                {
+                    id = this.ID
+                },
+                Text = text
+            };
+            Telegram.Send(reply);
+        }
+
+
+        private readonly Int32 id;
     }
 }
