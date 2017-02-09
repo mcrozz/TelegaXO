a1d89faac28d523ce3d47be17833d0a618c03692
Rozz <ivanzarudny@gmail.com>
2017-02-09 16:38:07 +0500

    Removed test code

--- Server/Server/Game.cs
+++ Server/Server/Game.cs
@@ -71,64 +71,6 @@ namespace Server
                         break;
                 }
             }
-
-            // For testing purposes only
-            // <<
-            // TODO: remove
-            if (message.text.Equals("Hey"))
-            {
-                DTO.Message reply = new DTO.Message
-                {
-                    User = message.@from,
-                    Text = "Good day"
-                };
-                Telegram.Send(reply);
-            }/*
-                else if (message.message.text.Equals("Test"))
-                {
-                    DTO.Telegram.KeyboardButton[,] buttons = new DTO.Telegram.KeyboardButton[2, 2]
-                    {
-                        {new DTO.Telegram.KeyboardButton("Hey"), new DTO.Telegram.KeyboardButton("Don't") },
-                        {new DTO.Telegram.KeyboardButton("Stop"), new DTO.Telegram.KeyboardButton("Test") }
-                    };
-                    DTO.Telegram.ReplyKeyboardMarkup keyboard = new DTO.Telegram.ReplyKeyboardMarkup
-                    {
-                        keyboard = buttons,
-                        one_time_keyboard = true,
-                        resize_keyboard = true
-                    };
-                    DTO.Message reply = new DTO.Message
-                    {
-                        User = message.message.@from,
-                        Text = "Choose wisely",
-                        KeyboardMarkup = keyboard
-                    };
-                    Telegram.Send(reply);
-                }*/
-            else if (message.text.Equals("Test"))
-            {
-                Int32 size = 8;
-                String[] XY = new String[] {"X", "O", " "};
-                Random random = new Random(new TimeSpan().Milliseconds);
-                Func<String> XorY = new Func<String>(() => XY[random.Next(0, 3)]);
-                DTO.Telegram.InlineKeyboardButton[,] buttons = new DTO.Telegram.InlineKeyboardButton[size, size];
-                for (Int32 i=0; i<size; i++)
-                    for (Int32 j = 0; j < size; j++)
-                        buttons[i, j] = new DTO.Telegram.InlineKeyboardButton(XorY(), $"{i}_{j}");
-
-                DTO.Telegram.InlineKeyboardMarkup keyboard = new DTO.Telegram.InlineKeyboardMarkup()
-                {
-                    inline_keyboard = buttons
-                };
-                DTO.Message reply = new DTO.Message()
-                {
-                    User = message.@from,
-                    Text = "Select one",
-                    KeyboardMarkup = keyboard
-                };
-                Telegram.Send(reply);
-            }
-            // >>
         }
 
         private static void ParseQueryUpdate(DTO.Telegram.CallbackQuery query)
