01246585f0c259f6624a3d790d1b484dbb87c777
floatGOAT <pandarpr@yandex.ru>
2017-02-09 18:09:40 +0600

    Add interface "IPlayer"
    This interface will be used for palyer and AI.

---
+++ Server/Server/IPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Server
+{
+    interface IPlayer
+    {
+        Int32 ID { get; }
+        void SendMessage(Room.Fill[,] cell);
+        void SendMessage(string text);
+    }
+}
