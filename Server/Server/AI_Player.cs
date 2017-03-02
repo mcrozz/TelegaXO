a6de35c5471e9c654fc7768893fafdfc37d7c302
floatGOAT <pandarpr@yandex.ru>
2017-03-02 21:07:33 +0600

    New feature: AI_Player
    Start developing new game mod "Player vs AI".

---
+++ Server/Server/AI_Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Hosting;
+using System.Web.UI.WebControls;
+using Server.DTO.Telegram;
+
+
+namespace Server
+{
+    class AI_Player : IPlayer
+    {
+
+        public AI_Player(Room.Fill Marker)
+        {
+            this.user.id = -1;
+            this.user.username = "bot";
+            this.marker = Marker;
+        }
+
+        public Int32 ID
+        {
+            get { return user.id; }
+        }
+
+        public void SendMessage(Room.Fill[,] board)
+        {
+            /*if (in one row/Column are two enemy markers)
+                IApplicationPreloadUtil own marker between them */
+            if (board[1, 1] == Room.Fill.empty)
+                board[1, 1] = marker;
+            // smth like "SendMessage", isn't it ?
+            else if (true)
+                return;
+            
+
+            DTO.Telegram.CallbackQuery BotReply = new DTO.Telegram.CallbackQuery
+            {
+                id = String.Empty,
+                from = this.user,
+                data = String.Concat('_')
+            };
+
+            Game.Queue(BotReply);
+        }
+
+        public void SendMessage(string text)
+        {
+            // Mock
+        }
+
+        private readonly DTO.Telegram.User user;
+        private readonly Room.Fill marker;
+    }
+}
